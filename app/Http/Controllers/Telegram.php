669bf220439c2f39b0179478a960634be2bce0fc
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2016-08-31 03:50:10 +0500

    [fix] мелочи

--- app/Http/Controllers/Telegram.php
+++ app/Http/Controllers/Telegram.php
@@ -38,14 +38,18 @@ class Telegram extends Controller {
 
     $this->user = User::find($this->request->message->from->id);
 
+    $userName   = property_exists($this->request->message->from, 'first_name') ? $this->request->message->from->first_name : null;
+    $userFamily = property_exists($this->request->message->from, 'last_name')  ? $this->request->message->from->last_name  : null;
+    $userNick   = property_exists($this->request->message->from, 'username')   ? $this->request->message->from->username   : null;
+
     // Такого пользователя мы еще не встречали. Добавим его в нашу базу.
     if (is_null($this->user))
     {
       $this->user = new User([
         'id'     => $this->request->message->from->id,
-        'name'   => property_exists($this->request->message->from, 'first_name') ? $this->request->message->from->first_name : null,
-        'family' => property_exists($this->request->message->from, 'last_name')  ? $this->request->message->from->last_name  : null,
-        'nick'   => property_exists($this->request->message->from, 'username')   ? $this->request->message->from->username   : null,
+        'name'   => $userName,
+        'family' => $userFamily,
+        'nick'   => $userNick,
       ]);
     
       $this->user->save();
@@ -57,6 +61,15 @@ class Telegram extends Controller {
             ->lists('id')
             ->toArray()
         );
+    }
+
+    // Если у пользователя обновились имя, фамилия или никнейм - обновим их и у себя в БД
+    if ($this->user->name !== $userName or $this->user->family !== $userFamily or $this->user->nick !== $userNick)
+    {
+      $this->user->name   = $userName;
+      $this->user->family = $userFamily;
+      $this->user->nick   = $userNick;
+      $this->user->save();
     }
 
     $bot_service = app('Bot');
